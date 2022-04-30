6d19ff2cb32462de0f19d55bfded1f557d308b8d
bradmag <104655927+bradmag@users.noreply.github.com>
2022-04-30 12:23:12 -0600

    Update Prioritized-Inventory
    Implemented basic methods
    +enqueue
    +dequeue
    +getSize

--- Prioritized-Inventory.java
+++ Prioritized-Inventory.java
@@ -1,1 +1,41 @@
 /** Design a priority queue that prioritizes food that is about to go bad */
+
+/** This queue will be the Inventory list that keeps track of perishable food */
+public class PriorityQueue<Food> {
+	private Heap<Food> fifoHeap;
+	
+	/**Default Constructor */
+	public PriorityQueue() {
+		fifoHeap = new Heap<Food>();
+	}
+	
+	/** Constructor that accepts a Comparator as a parameter */
+	public PriorityQueue(java.util.Comparator<Food> c) {
+		fifoHeap = new Heap<Food>(c);
+	}
+	
+	/** Add a new object to the queue */
+	public void enqueue(Food newObject) {
+		fifoHeap.add(newObject);
+	}
+	
+	/** Return and Remove Highest priority item */
+	public Food dequeue() {
+		return fifoHeap.remove();
+	}
+	
+	/** Returns number of elements in the queue */
+	public int getSize() {
+		return fifoHeap.getSize();
+	}
+	
+	
+	//TODO implement next3 and next5 to print the next few items in the queue
+	
+	
+	@Override
+	public String toString() {
+		//TODO fix this
+		return fifoHeap.toString();
+	}
+}
