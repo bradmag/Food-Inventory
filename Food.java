ee94ca926927179e8392508b36c9c3c97d7e84be
bradmag <104655927+bradmag@users.noreply.github.com>
2024-01-04 16:09:35 -0700

    Restart Food.java
    Create a Food class to act as a parent class.
    Basic constructor to store name and expiration date

--- Food.java
+++ Food.java
@@ -1,23 +1,20 @@
-public abstract class Food implements IsEddible, Comparable<Food> {
-	private int daysTillEXP; //Days until the food expires
-	
-	/** returns number of days until the food expires */
-	public int getEXP(){ 
-		return this.daysTillEXP;
-	}
-	
-	/** Set number of days until food expires */
-	public void setEXP(int exp) {
-		this.daysTillEXP = exp;
-	}
-	
-	@Override /**returns Positive 1  if expires sooner, return -1 if expires after*/
-	public int compareTo(Food o) {
-		if(getEXP() < o.getEXP())
-			return 1;
-		else if (getEXP() > o.getEXP())
-			return -1;
-		else
-			return 0;	
-	}
+import java.util.Date;
+
+public class Food {
+    String name;
+    Date expiryDate;
+
+    public Food(String name, Date expiryDate) {
+        this.name = name;
+        this.expiryDate = expiryDate;
+    }
+
+    public void eat() {
+        System.out.println("Yum! food is good!");
+    }
+
+    @Override
+    public String toString() {
+        return name + " (Expires on: " + expiryDate + ")";
+    }
 }
